b0382ca855a61558e3b91fbd61ebd6cd2e67d146
Cam Smith <cameren.e.smith@gmail.com>
2025-06-01 10:22:38 -0400

    cleanup files

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\camer\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe" -m PyInstaller --onefile --windowed --add-data "assets;assets" --add-data "data;data" --icon "assets/images/supermaultd.png" main.py
-pause 
